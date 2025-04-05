eea1b449749e3bf7e67312e7cb64181d628512bc
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-04-05 15:42:00 +0200

    Sound don't work

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -4,8 +4,15 @@ extends RigidBody2D
 @export var axis:Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+	var aBodies : Array[Node2D] = get_colliding_bodies()
+	
+	if aBodies.size() > 0 :
+		for bodyNode2D : Node2D in aBodies :
+			if bodyNode2D.is_in_group("Wall"):
+				print("touch")
+				$WallHit.Play();
+	
 	move(delta)
-
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
