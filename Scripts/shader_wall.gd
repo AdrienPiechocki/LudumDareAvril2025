bb26656667edf22ba19264337486ae94ff7c434c
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-04-05 13:12:02 +0200

    Adding wall to Game Scene

--- Scripts/shader_wall.gd
+++ Scripts/shader_wall.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 @export var rTexture : Resource
+@export var iXSize : int
+@export var iYSize : int
 
 func _ready():
 	$TextureRect.texture = rTexture
+	$TextureRect.size.x = iXSize
+	$TextureRect.size.y = iYSize
+	
+	$CollisionShape2D.shape.size.x = iXSize
+	$CollisionShape2D.shape.size.y = iYSize
