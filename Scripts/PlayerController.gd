28dacc1291b4e588621309fa4c13e898b94f9024
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-04-06 17:14:09 +0200

    Camera Shake

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -7,8 +7,36 @@ extends RigidBody2D
 var coin = preload("res://Prefabs/FallingCoin.tscn")
 var hitParticlues = preload("res://Prefabs/Particules/BucketHitParticules.tscn")
 
+# How quickly to move through the noise
+var _fNOISE_SHAKE_SPEED: float = 30.0
+# Noise returns values in the range (-1, 1)
+# So this is how much to multiply the returned value by
+var _fNOISE_SHAKE_STRENGTH: float = 10.0
+# Multiplier for lerping the shake strength to zero
+var _fSHAKE_DECAY_RATE: float = 2.0
+
+@onready var _camera : Camera2D
+@onready var _rand = RandomNumberGenerator.new()
+@onready var _noise = FastNoiseLite.new()
+
+var _fNoise: float = 0.0
+var _fShakeStrength: float = 0.0
+
 var _bHaveCoin : bool = false
 var flag:bool = true
+
+func _ready() -> void:
+	_camera = get_node("../../Camera2D")
+	_rand.randomize()
+	_noise.seed = _rand.randi()
+	# Period affects how quickly the noise changes values
+
+func _process(delta: float) -> void:
+	# Fade out the intensity over time
+	_fShakeStrength = lerp(_fShakeStrength, .0, _fSHAKE_DECAY_RATE * delta)
+
+	# Shake by adjusting camera.offset so we can move the camera around the level via it's position
+	_camera.offset = get_noise_offset(delta)
 
 func _physics_process(delta: float) -> void:
 	var aBodies : Array[Node2D] = get_colliding_bodies()
@@ -18,7 +46,7 @@ func _physics_process(delta: float) -> void:
 			if bodyNode2D.is_in_group("Obstacles") && flag:
 				cooldown()
 				$WallHit.play()
-				
+				apply_noise_shake()
 				var contact_point : Vector2 = (to_local(global_position) + to_local(bodyNode2D.global_position)) / 2
 				var hitParticluesInstance : CPUParticles2D = hitParticlues.instantiate()
 				hitParticluesInstance.position = contact_point
@@ -77,3 +105,15 @@ func losingCoin():
 	add_child(coinInstance)
 	coins -= 1
 	_bHaveCoin = coins <= 0
+
+func get_noise_offset(delta: float) -> Vector2:
+	_fNoise += delta * _fNOISE_SHAKE_SPEED
+	# Set the x values of each call to 'get_noise_2d' to a different value
+	# so that our x and y vectors will be reading from unrelated areas of noise
+	return Vector2(
+		_noise.get_noise_2d(1, _fNoise) * _fShakeStrength,
+		_noise.get_noise_2d(100, _fNoise) * _fShakeStrength
+	)
+
+func apply_noise_shake() -> void:
+	_fShakeStrength = _fNOISE_SHAKE_STRENGTH
