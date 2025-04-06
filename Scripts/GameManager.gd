ba5ef65c36fe001c48f53f464553c75de9490f70
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-04-06 10:34:53 +0200

    RockParticules

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var coin = preload("res://Prefabs/Coin.tscn")
 var rock = preload("res://Prefabs/Rock.tscn")
+var rockParticules = preload("res://Prefabs/Particules/FallingRockParticules.tscn")
 var bat = preload("res://Prefabs/Bat.tscn")
 var rng = RandomNumberGenerator.new()
 
@@ -11,23 +12,33 @@ func _ready() -> void:
 	spawnBat()
 
 func spawnBat():
-	var instance = bat.instantiate()
-	instance.position = Vector2(rng.randf_range(50, 70), 300)
-	add_child(instance)
+	var batInstance = bat.instantiate()
+	batInstance.position = Vector2(rng.randf_range(50, 70), 300)
+	add_child(batInstance)
 	await get_tree().create_timer(rng.randf_range(5,10)).timeout
 	spawnBat()
 
 
 func spawnRock():
-	var instance = rock.instantiate()
-	instance.position = Vector2(rng.randf_range(20,120), -20)
-	add_child(instance)
+	
+	var iRNG = Vector2(rng.randf_range(20,120), -20)
+	
+	var rockParticulesInstance = rockParticules.instantiate()
+	rockParticulesInstance.position = iRNG
+	add_child(rockParticulesInstance)
+	rockParticulesInstance.emitting = true
+	await get_tree().create_timer(1).timeout
+	
+	var rockInstance = rock.instantiate()
+	rockInstance.position = iRNG
+	add_child(rockInstance)
+	
 	await get_tree().create_timer(rng.randf_range(4,8)).timeout
 	spawnRock()
 	
 func spawnCoin():
-	var instance = coin.instantiate()
-	instance.position = Vector2(rng.randf_range(20, 120), 300)
-	add_child(instance)
+	var coinInstance = coin.instantiate()
+	coinInstance.position = Vector2(rng.randf_range(20, 120), 300)
+	add_child(coinInstance)
 	await get_tree().create_timer(rng.randf_range(2,5)).timeout
 	spawnCoin()
